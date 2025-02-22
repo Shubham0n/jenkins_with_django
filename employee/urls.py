c871756fd97e5b239d9d8829b43309e51c16e87d
ps-bitcoding <ps.bitcoding@gmail.com>
2025-02-22 10:26:48 +0530

    fix: changes in employee module

--- employee/urls.py
+++ employee/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", emp_list, name="emp_list"),
     path("create/", emp_create, name="emp_create"),
     path("<slug:employ>/", emp_info, name="emp_info"),
-    path("<slug:slug>/update/", emp_update, name="emp_update"),
-    path("<slug:slug>/delete/", emp_delete, name="emp_delete"),
+    # path("<slug:slug>/update/", emp_update, name="emp_update"),
+    # path("<slug:slug>/delete/", emp_delete, name="emp_delete"),
 ]
